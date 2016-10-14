3716e01c802efc59810d72094506374a04facd32
leeway <liwei@bolo.me>
2016-10-14 11:06:13 +0800

    rename src to http_mocker

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,7 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-from src import models
+from http_mocker import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -12,17 +12,7 @@ config = context.config
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-
-
 target_metadata = models.Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline():
